dfbd90c1b6e4888c4a6a5a2467745629de46b74e
Siddharth Sonawane <sidharthsonavane@gmail.com>
2021-07-01 11:29:39 +0530

    final v1

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 var startBtn = document.getElementById("start-btn");
 var stopBtn = document.getElementById("stop-btn");
 
-var barStyle = {
-  height: "100%",
-  transitionProperty: "height",
-  transitionDuration: "2s",
-};
-
 var elements = document.getElementsByClassName("bar");
 
 startBtn.addEventListener("click", function () {
   for (var i = 0, length = elements.length; i < length; i++) {
-    Object.assign(elements[i].style, barStyle);
+    elements[i].style.animation = "equalize 10s 0s infinite";
+    elements[0].style.animationDelay = "-1.9s";
+    elements[1].style.animationDelay = "-2s";
+    elements[2].style.animationDelay = "-2.3s";
+    elements[3].style.animationDelay = "-2.4s";
+    elements[4].style.animationDelay = "-2.1s";
+    elements[0].style.transform = "translate(0,0)";
   }
 });
 
 stopBtn.addEventListener("click", function () {
   for (var i = 0, length = elements.length; i < length; i++) {
-    elements[i].style.height = "1%";
-    elements[i].style.transitionProperty = "height";
-    elements[i].style.transitionDuration = "2s";
+    elements[i].style.animationPlayState = "paused";
   }
 });
-
-//document.getElementById("myDIV").style.animationIterationCount = "infinite";
