94c09dd9f91b9483078467fc57f2edf12cdfbe62
Siddharth Sonawane <sidharthsonavane@gmail.com>
2021-07-01 13:04:38 +0530

    update to final

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ var elements = document.getElementsByClassName("bar");
 
 startBtn.addEventListener("click", function () {
   for (var i = 0, length = elements.length; i < length; i++) {
-    elements[i].style.animation = "equalize 10s 0s infinite";
-    elements[0].style.animationDelay = "-1.9s";
+    elements[i].style.animation = "equalize 7s 0s infinite";
+    elements[0].style.animationDelay = "-1.3s";
     elements[1].style.animationDelay = "-2s";
-    elements[2].style.animationDelay = "-2.3s";
+    elements[2].style.animationDelay = "-5.3s";
     elements[3].style.animationDelay = "-2.4s";
-    elements[4].style.animationDelay = "-2.1s";
-    elements[0].style.transform = "translate(0,0)";
+    elements[4].style.animationDelay = "-3s";
   }
 });
 
